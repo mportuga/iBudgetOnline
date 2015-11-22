5f10fa5e11a9c6b2675ece3d8b91471915475cbf
Marcelo Sauerbrunn Portugal <marceloquarion@gmail.com>
2015-11-22 16:48:00 -0500

    As a user I can now see a grid on the accounts page

--- public/javascripts/accounts.js
+++ public/javascripts/accounts.js
@@ -1,1 +1,15 @@
-function initPage() {}
+function getTableOptions(){
+  return {
+    ajax: {
+      url: "/budget/data/get",
+      data: {
+        dataType: 'grid',
+        columns: [ "date", "payee", "status", "debt", "credit", "balance"]
+      }
+    }
+  };
+}
+
+function initPage() {
+  $('#accountsTable').DataTable(getTableOptions());
+}
